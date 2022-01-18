47855d1831132c03499461b21c06d1a1ebf3de10
Sungjun Choi <sungjun0110@gmail.com>
2022-01-17 19:24:01 -0800

    adds authentication

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # import HttpResponse to test view functions
 # will delete after imlementing templates
@@ -7,4 +11,23 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>test</h1>')
+    return render(request, 'home.html')
+
+def kits_index(request):
+    return render(request, 'covidtracker/index.html')
+
+
+def signup(request):
+    error_mesage = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            # This will add the user to the database
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+        else:
+            error_message = 'Invalid sign up - try again'
+    form = UserCreationForm()
+    context = {'form': form, 'error-message': error_mesage}
+    return render(request, 'registration/signup.html', context)
