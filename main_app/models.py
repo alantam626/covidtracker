e45e0a2fcddf9cc0194f3ef1ed2e829475d8bc87
Alan Tam <tam.alan7@gmail.com>
2022-01-18 10:11:48 -0800

    adds prelim models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,14 @@
+from re import S
 from django.db import models
 
 # Create your models here.
+
+class Strategy(models.Model):
+    name = models.CharField(max_length=100)
+    rating = models.CharField(max_length=50)
+    type = models.CharField(max_length=50)
+    
+class Kit(models.Model):
+    date = models.DateField()
+    strategy = models.ManyToManyField(Strategy)
+    
